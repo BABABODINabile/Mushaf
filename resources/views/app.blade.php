a6d460df62966ed217f28b80911afbdf4ff453b1
BABABODINabile <bababodinabile@gmail.com>
2026-09-10 18:46:00 +0100

    feat(seo): favicon du site et meta theme-color dans le layout racine

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="theme-color" content="#0f766e">
         <title inertia>{{ config('app.name', 'Mushaf') }}</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
         <link rel="preconnect" href="https://fonts.bunny.net">
+        @if(config('services.r2.public_url'))
+            <link rel="preconnect" href="{{ config('services.r2.public_url') }}" crossorigin>
+        @endif
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.jsx'])
         @inertiaHead
